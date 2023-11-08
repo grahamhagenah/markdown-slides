bd5425fe51e9206db5c4f0d2ac800d12cae6be7b
Graham Hagenah <gwhagenah@gmail.com>
2023-11-08 10:14:10 -0500

    adding legend functionality

--- src/App.js
+++ src/App.js
@@ -2,35 +2,42 @@ import React, { useState } from 'react';
 import ReactMarkdown from 'react-markdown';
 import gfm from 'remark-gfm';
 import MarkdownIt from 'markdown-it';
-import MdEditor from 'react-markdown-editor-lite';
+import MdEditor, { Plugins } from 'react-markdown-editor-lite';
 import 'react-markdown-editor-lite/lib/index.css';
+import logo from './logo.svg';
 
 // Initialize a markdown parser
 const mdParser = new MarkdownIt(/* Markdown-it options */);
 
+// Remove options from editor 
+MdEditor.unuse(Plugins.FullScreen); // header
+MdEditor.unuse(Plugins.ModeToggle); // mode toggle
+
 function App() {
+  
   // const [markdown, setMarkdown] = useState();
   const [slides, setSlides] = useState([])
+  const [legend, setLegend] = useState([])
   // const [currentSlide, setCurrentSlide] = useState(0)
 
-   // Accessing the contents of the `slides` state variable
+  // Accessing the contents of the `slides` state variable
   const currentSlides = slides;
 
-  // Function to change slides based on some condition or input
-  // const changeSlide = (newMarkdown) => {
-  //   setMarkdown(newMarkdown);
-  // };
-  
-  function handleEditorChange({ html, text }) {
+  // Accessing the contents of the `legend` state variable
+  const currentLegend = legend;
 
+  
+  function handleEditorChange({ text }) {
     // Split the markdown text by lines
     const lines = text.split('\n');
+    const legend = [];
     const sections = [];
     let currentSection = [];
     
     lines.forEach((line, index) => {
       if (line.startsWith('## ') && currentSection.length > 0) {
-        // When a new H2 heading is found, join the current section lines and push to sections
+        legend.push(line)
+        // When a new H2 heading is found, push the title to the legend, then join the current section lines and push to sections
         sections.push(currentSection.join('\n'));
         // Reset the current section
         currentSection = [line];
@@ -45,24 +52,29 @@ function App() {
       }
     });
   
-    console.log(sections);
-  
-    setSlides(sections);
-
-}
+    console.log("sections: " + sections);
+    console.log("legend: " + legend);
+    setSlides(sections)
+    setLegend(legend)
+  }
   
   return (
     <div className="App">
+      <img src={logo} className="logo" alt="Logo" />
       <div className="slide-view">
-       <div className="slides">
-          {currentSlides.map((slide, index) => (
-            <ReactMarkdown className="slide" key={index} remarkPlugins={[gfm]} children={slide}/>
-          ))}
+        <div className="legend">
+          {currentLegend.map((title, index) => (
+              <ReactMarkdown className="title" key={index} remarkPlugins={[gfm]} children={title} />
+            ))}
         </div>
-        {/* <button onClick={() => changeSlide("# Slide 2\n\nThis is the second slide content.")}>Next Slide</button> */}
+        <div className="slides">
+            {currentSlides.map((slide, index) => (
+              <ReactMarkdown className="slide" key={index} remarkPlugins={[gfm]} children={slide} />
+            ))}
+          </div>
       </div>
       <div className="markdown-view">
-        <MdEditor style={{ height: '100vh' }} renderHTML={text => mdParser.render(text)} onChange={handleEditorChange} />
+        <MdEditor style={{ height: '100vh' }} renderHTML={text => mdParser.render(text)} onChange={handleEditorChange} enablePreview={false} />
       </div>
     </div>
   );
