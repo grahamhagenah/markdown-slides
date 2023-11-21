403ffd780c2183d6d91ce9dcb1a08338b3f6ef42
Graham Hagenah <gwhagenah@gmail.com>
2023-11-21 16:07:46 -0500

    automatically change slides when editing certain sections of markdown

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import ReactMarkdown from 'react-markdown'
 import gfm from 'remark-gfm'
 import MarkdownIt from 'markdown-it'
@@ -6,31 +6,52 @@ import MdEditor, { Plugins } from 'react-markdown-editor-lite'
 import 'react-markdown-editor-lite/lib/index.css'
 import logo from './logo.svg'
 import { instructions } from './instructions.js'
+import classNames from 'classnames/bind';
 
 // Initialize a markdown parser
-const mdParser = new MarkdownIt(/* Markdown-it options */)
+const mdParser = new MarkdownIt(/* Markdown-it options */);
 
 // Remove options from editor 
 MdEditor.unuse(Plugins.FullScreen) // full screen
 MdEditor.unuse(Plugins.ModeToggle) // mode toggle
 
-function App() {
+const App = () => {
   
   const [slides, setSlides] = useState([])
+  const [beforeEditSlides, setbeforeEditSlides] = useState([])
+  // const [inputValue, setInputValue] = useState("example value")
   const [current, setCurrent] = useState([0])
   const [legend, setLegend] = useState([])
 
-  // Accessing the contents of the `slides` state variable
+  // Accessing the contents of the state variables
   const currentSlide = current
-
-  // Accessing the contents of the `slides` state variable
   const currentSlides = slides
-
-  // Accessing the contents of the `legend` state variable
+  const beforeEdit = beforeEditSlides
   const currentLegend = legend
 
   
+  function findChangedIndex(before, after) {
+    // Check if the arrays are of different lengths
+    if (before.length !== after.length) {
+      return -1; // Indicates that the arrays have different lengths
+    }
+  
+    // Compare the elements of the arrays
+    for (let i = 0; i < before.length; i++) {
+      if (before[i] !== after[i]) {
+        return i; // Return the index of the first difference found
+      }
+    }
+    // If the arrays are identical
+    return -1; // Indicates that there are no changes
+  }
+
+
   function handleEditorChange({ text }) {
+
+    // Captures state of current slides for later comparison
+    setbeforeEditSlides(currentSlides)
+
     // Split the markdown text by lines
     const lines = text.split('\n')
     const legend = []
@@ -44,7 +65,6 @@ function App() {
         sections.push(currentSection.join('\n'))
         // Reset the current section
         currentSection = [line]
-        console.log("before" + sections)
         // First encounter, likely an H1 with no content in currentSection
       } else if (((line.startsWith('# ') || line.startsWith('## ')) && (currentSection.length === 0))) {
         legend.push(line)
@@ -62,23 +82,24 @@ function App() {
 
     setSlides(sections)
     setLegend(legend)
+    setCurrent(findChangedIndex(beforeEdit, currentSlides))
   }
-  
+
   return (
     <div className="App">
       <img src={logo} className="logo" alt="Logo" />
       <div className="slide-view">
         <div className="legend">
           {currentLegend.map((title, index) => (
-             <div className="title-container" onClick={() => setCurrent(index)} >
-                <ReactMarkdown className="title" key={index} remarkPlugins={[gfm]} children={title} />
+             <div id={'slide-' + index} className="title-container" onClick={() => setCurrent(index)} >
+                <ReactMarkdown className={(currentSlide == index) ? ('current-slide title')  : 'title'}  key={index} remarkPlugins={[gfm]} children={title} />
               </div>
             ))}
         </div>
         <ReactMarkdown className="slide" remarkPlugins={[gfm]} children={currentSlides[currentSlide]} />
       </div>
       <div className="markdown-view">
-        <MdEditor id="editor" style={{ height: '100%' }} renderHTML={text => mdParser.render(text)} placeholder={instructions} view={{ menu: true, md: true, html: false }} onChange={handleEditorChange} />
+        <MdEditor id="editor" style={{ height: '100%' }}  view={{ menu: true, md: true, html: false }} renderHTML={text => mdParser.render(text)} onChange={handleEditorChange} />
       </div>
     </div>
   );
