e1449b28f833ab4afd5179695eab861b20cc463a
Graham Hagenah <gwhagenah@gmail.com>
2023-11-22 00:34:25 -0500

    adding more instructions

--- src/instructions.js
+++ src/instructions.js
@@ -1,21 +1,29 @@
-export const instructions = `# Introducing Markdown Slides
+export const instructions = `# Introducing Markdown Slides 
 
-Markdown is a lightweight markup language that allows you to format plain text documents. It's easy to learn and widely used for creating documents, web pages, and files. 
+Markdown is a lightweight markup language that allows you to format plain text documents. It's easy to learn and widely used for creating documents and web pages. 
 
-Now you can make **presentations** in markdown.
+Now you can make **presentations** in markdown. This web application transforms the markup language into simple slide decks to draft, share, or present. 
 
+Read on to see how it works, or edit this document to try it yourself :‑)
 
 ## Creating New Slides
 
-The H1 and H2 tags mark the beginning of new slides.
+The H1 and H2 tags mark the beginning of new slides. Each heading is denoted by hashtags (#) to establish hierarchy in the document.
 
+Type in the hashtags manually or use the formatting menu to create new headings. 
+
+If you make a mistake, there's an undo button too.
 
 ## Basic Formatting
 
 You can use basic formatting to make text **bold**, *italic*, or ~~strikethrough~~.
 
+You can create [hyperlinks](https://grahamhagenah.com) by enclosing the link text in square brackets and the URL in parentheses.
 
-## Lists
+To create a blockquote, add a > in front of a paragraph.
+> No one is useless in this world who lightens the burdens of another.
+
+## Making Lists
 
 ### Ordered List
 1. Item 1
@@ -27,7 +35,20 @@ You can use basic formatting to make text **bold**, *italic*, or ~~strikethrough
 - Item B
 - Item C
 
+## Writing Code 
+Display code using plain HTML. Just make sure to indent with four spaces.
 
-## Links
-You can create [hyperlinks](https://www.example.com) by enclosing the link text in square brackets and the URL in parentheses.
+    <html>
+      <head>
+        <title>Title here</title>
+      </head>
+      
+## How It's Built
+Markdown Slides is built using **React**, a front-end library for creating user-interfaces.
+
+The markdown editor is provided by [react-markdown-editor-lite](https://www.npmjs.com/package/react-markdown-editor-lite).
+
+If you have questions or suggestions, [contact me](https://grahamhagenah.com/about/). I'm open to work.
+
 `
+
