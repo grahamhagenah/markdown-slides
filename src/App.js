a52a434fc18057edb927fdce5e4a24bc492b764e
Graham Hagenah <gwhagenah@gmail.com>
2023-11-24 14:39:08 -0500

    moving demo variable to its own file and exporting

--- src/App.js
+++ src/App.js
@@ -12,70 +12,7 @@ import ArrowBackIcon from '@mui/icons-material/ArrowBack'
 import IconButton from '@mui/material/IconButton'
 import FullscreenExitIcon from '@mui/icons-material/FullscreenExit'
 import AddIcon from '@mui/icons-material/Add';
-
-let demo = "# Introducing *Markdown Slides*\n" + "---\n" + "---\n" +
-"Markdown is a lightweight markup language that allows you to format plain text documents. It is easy to learn and widely used for creating documents and web pages.\n\n" +
-"Now you can make **presentations** in markdown. This web application transforms the markup language into simple slide decks to draft, share, or present.\n\n" +
-"Read on to learn more or start making slides in the markdown editor :‑)\n\n" +
-"## Creating New Slides\n\n" +
-"The H1 and H2 tags mark the beginning of new slides. Each heading is denoted by hashtags (#) to establish hierarchy in the document.\n\n" +
-"Type in the hashtags manually or use the formatting menu to create new headings." +
-"\n" + "\n" +
-"If you make a mistake, there's an undo button too." +
-"\n" + "\n" +
-"## Basic Formatting" +
-"\n" + "\n" +
-"You can use basic formatting to make text **bold**, *italic*, or ~~strikethrough~~." +
-"\n" + "\n" +
-"You can create [hyperlinks](https://grahamhagenah.com) by enclosing the link text in square brackets and the URL in parentheses." +
-"\n" + "\n" +
-"To create a blockquote, add a > in front of a paragraph." +
-"\n" + "\n" +
-"> No one is useless in this world who lightens the burdens of another." +
-"\n" + "\n" +
-"You can write `` inline code `` as well." + "\n" +
-"\n" + "\n" +
-"\n" + "\n" +
-"## Making Lists" +
-"\n" + "\n" +
-"### Ordered List" +
-"\n" + "\n" +
-"1. Item 1" +
-"\n" +
-"2. Item 2" +
-"\n" +
-"3. Item 3" +
-"\n" +
-"### Unordered List" +
-"\n" + "\n" +
-"- Item A" + "\n" +
-"- Item B" + "\n" +
-"- Item C" + "\n" +
-"\n" + "\n" +
-"## Using Controls" +
-"\n" + "\n" +
-"Enter a distraction-free *fullscreen* mode by using the toggle below." +
-"\n" + "\n" + "Navigate between slides using the left and right arrows, or use the arrow keys (← ↑ → ↓) on your keyboard." +
-"\n" + "\n" +
-"## Tabular Data" +
-"\n" + "\n" +
-"Create a table of varying sizes from the menu above the editor." + 
-"\n" + "\n" +
-"| Title | Author | Year |" + "\n" +
-"|-------|--------|------|" + "\n" +
-"| The Stone Sky | N. K. Jemisin | 2018" + "\n" +
-"| The Three-Body Problem  | Cixin Liu | 2015" + "\n" +
-"| Rainbows End  | Vernor Vinge | 2007" + "\n" +
-"| Spin  | Robert Charles Wilson | 2006" + "\n" +
-"| Speaker for the Dead | Orson Scott Card | 1987" + "\n" +
-"\n" + "\n" +
-"## How It's Built" +
-"\n" + "\n" + 
-"Markdown Slides is built using **React**, a front-end library for creating user-interfaces." +
-"\n" + "\n" + 
-"The markdown editor is provided by [react-markdown-editor-lite](https://www.npmjs.com/package/react-markdown-editor-lite)." +
-"\n" + "\n" + 
-"If you have questions or suggestions, [contact me](https://grahamhagenah.com/about/). I'm open to work."
+import {demo} from './demo.js'
 
 // Initialize a markdown parser
 const mdParser = new MarkdownIt(/* Markdown-it options */)
@@ -161,14 +98,17 @@ export default function App() {
     // If a new slide has been found, set current to the new slide
     if(sections.lastIndexOf('## New Slide\nAdd slide content here') ===  sections.length-1) {
       setCurrent(sections.length-1)
+      let lastListItem = document.querySelector("ol").lastElementChild
+      // Set the tabindex to make it focusable (if it's not already)
+      lastListItem.tabIndex = 0;
       // Shift focus to new slide
-      document.querySelector("ol").lastElementChild.focus();
+      lastListItem.focus();
     }
     // Otherwise set current to the latest edit
     else {
       setCurrent(findChangedIndex(stash, currentSlides))
     }
-    
+
     setSlides(sections)
     setLegend(legend)
   }
--- src/demo.js
+++ src/demo.js
@@ -1,63 +1,69 @@
-export const demo = "# Introducing *Markdown Slides*\n" + "---\n" + "---\n" +
-"Markdown is a lightweight markup language that allows you to format plain text documents. It is easy to learn and widely used for creating documents and web pages.\n\n" +
-"Now you can make **presentations** in markdown. This web application transforms the markup language into simple slide decks to draft, share, or present.\n\n" +
-"Read on to learn more or start making slides in the markdown editor :‑)\n\n" +
-"## Creating New Slides\n\n" +
-"The H1 and H2 tags mark the beginning of new slides. Each heading is denoted by hashtags (#) to establish hierarchy in the document.\n\n" +
-"Type in the hashtags manually or use the formatting menu to create new headings." +
-"\n" + "\n" +
-"If you make a mistake, there's an undo button too." +
-"\n" + "\n" +
-"## Basic Formatting" +
-"\n" + "\n" +
-"You can use basic formatting to make text **bold**, *italic*, or ~~strikethrough~~." +
-"\n" + "\n" +
-"You can create [hyperlinks](https://grahamhagenah.com) by enclosing the link text in square brackets and the URL in parentheses." +
-"\n" + "\n" +
-"To create a blockquote, add a > in front of a paragraph." +
-"\n" + "\n" +
-"> No one is useless in this world who lightens the burdens of another." +
-"\n" + "\n" +
-"## Making Lists" +
-"\n" + "\n" +
-"### Ordered List" +
-"\n" + "\n" +
-"1. Item 1" +
-"\n" +
-"2. Item 2" +
-"\n" +
-"3. Item 3" +
-"\n" +
-"### Unordered List" +
-"\n" + "\n" +
-"- Item A" + "\n" +
-"- Item B" + "\n" +
-"- Item C" + "\n" +
-"\n" + "\n" +
-"## Using Controls" +
-"\n" + "\n" +
-"Enter a distraction-free *fullscreen* mode by using the toggle below." +
-"\n" + "\n" + "Navigate between slides using the left and right arrows, or use the arrow keys (← ↑ → ↓) on your keyboard." +
-"\n" + "\n" +
-"## Tabular Data" +
-"\n" + "\n" +
-"Create a table of varying sizes from the menu above the editor." + 
-"\n" + "\n" +
-"| Title | Author | Year |" + "\n" +
-"|-------|--------|------|" + "\n" +
-"| The Stone Sky | N. K. Jemisin | 2018" + "\n" +
-"| The Three-Body Problem  | Cixin Liu | 2015" + "\n" +
-"| Rainbows End  | Vernor Vinge | 2007" + "\n" +
-"| Spin  | Robert Charles Wilson | 2006" + "\n" +
-"| Speaker for the Dead | Orson Scott Card | 1987" + "\n" +
-"\n" + "\n" +
-"## Writing Code" + "\n" +
-"You can write `` inline code `` as well." + "\n" +
-"\n" + "\n" +
-"## How It's Built" +
-"\n" + "\n" + 
-"Markdown Slides is built using **React**, a front-end library for creating user-interfaces." +
-"\n" + "\n" + 
-"The markdown editor is provided by [react-markdown-editor-lite](https://www.npmjs.com/package/react-markdown-editor-lite)." +
-"\n" + "\n" + 
-"If you have questions or suggestions, [contact me](https://grahamhagenah.com/about/). I'm open to work."
+const demo = `# Introducing *Markdown Slides*
+---
+---
+Markdown is a lightweight markup language that allows you to format plain text documents. It is easy to learn and widely used for creating documents and web pages.
+
+Now you can make **presentations** in markdown. This web application transforms the markup language into simple slide decks to draft, share, or present.
+
+Read on to learn more or start making slides in the markdown editor :‑)
+
+## Creating New Slides
+
+The H1 and H2 tags mark the beginning of new slides. Each heading is denoted by hashtags (#) to establish hierarchy in the document.
+
+Type in the hashtags manually or use the formatting menu to create new headings.
+
+If you make a mistake, there's an undo button too.
+
+## Basic Formatting
+
+You can use basic formatting to make text **bold**, *italic*, or ~~strikethrough~~.
+
+You can create [hyperlinks](https://grahamhagenah.com) by enclosing the link text in square brackets and the URL in parentheses.
+
+To create a blockquote, add a > in front of a paragraph.
+
+> No one is useless in this world who lightens the burdens of another.
+
+## Making Lists
+
+### Ordered List
+
+1. Item 1
+2. Item 2
+3. Item 3
+### Unordered List
+
+- Item A
+- Item B
+- Item C
+
+
+## Using Controls
+
+Enter a distraction-free *fullscreen* mode by using the toggle below.
+
+Navigate between slides using the left and right arrows, or use the arrow keys (← ↑ → ↓) on your keyboard.
+
+## Tabular Data
+
+Create a table of varying sizes from the menu above the editor.
+
+| Title | Author | Year |
+|-------|--------|------|
+| The Stone Sky | N. K. Jemisin | 2018
+| The Three-Body Problem  | Cixin Liu | 2015
+| Rainbows End  | Vernor Vinge | 2007
+| Spin  | Robert Charles Wilson | 2006
+| Speaker for the Dead | Orson Scott Card | 1987
+
+
+## How It's Built
+
+Markdown Slides is built using **React**, a front-end library for creating user-interfaces.
+
+The markdown editor is provided by [react-markdown-editor-lite](https://www.npmjs.com/package/react-markdown-editor-lite).
+
+If you have questions or suggestions, [contact me](https://grahamhagenah.com/about/). I'm open to work.`
+
+export { demo }
